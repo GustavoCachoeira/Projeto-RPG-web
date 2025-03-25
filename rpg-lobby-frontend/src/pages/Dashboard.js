4284e23e102f68c463435a4e274de703ed17717a
GustavoCachoeira <gustavo.f.cachoeira.oficial@gmail.com>
2025-03-24 22:31:45 -0300

    Update Dashboard.js

--- rpg-lobby-frontend/src/pages/Dashboard.js
+++ rpg-lobby-frontend/src/pages/Dashboard.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import ConfirmModal from '../components/ConfirmModal'; // Importa o novo componente
+import ConfirmModal from '../components/ConfirmModal';
 
 function Dashboard() {
   const [user, setUser] = useState(null);
